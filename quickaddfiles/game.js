f0a4a0b3ba885c1bcadee57851840d7d8c1c804f
Tejesh1012 <125961420+Tejesh1012@users.noreply.github.com>
2024-06-07 23:25:10 +0530

    Update game.js

--- quickaddfiles/game.js
+++ quickaddfiles/game.js
@@ -7,3 +7,4 @@ let targetNum = Math.floor(Math.random() * maximum) + 1;
 console.log(targetNum)
 
 console.log("undo changes for all the java script files")
+console.log("undo changes for all the java script files")
